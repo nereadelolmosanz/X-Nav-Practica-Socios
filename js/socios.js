a4e1ae403affb2746417900336455ec66fb607d3
nereadelolmosanz <nereadelolmosanz@gmail.com>
2017-04-07 18:47:50 +0200

    primera funcionalidad añadida

--- js/socios.js
+++ js/socios.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
   var loaded = false;
+  var myline = false;
 
-  var create_msg = function(img,author,date,content,likes){
+  var create_msg = function(img,author,title,date,content,likes){
     msg = '<div class="OneMessage">';
     msg += '<img class="img-circle pull-left" src="'+img+'" width="50px" height="50px">';
     msg += '<div class="message-info">';
-    msg += '<small class="user-name text-primary">'+author+'</small>';
-    msg += '<small class="pull-right text-muted"><i class="fa fa-calendar"></i> '+date+'</small>';
+    msg += '<p><span class="msg-title text-primary">'+title+'</span>';
+    msg += ' -<span class="user-name text-primary">'+author+'</span>';
+    msg += '<small class="pull-right text-muted"><i class="fa fa-calendar"></i> '+date+'</small></p>';
     msg += '<p class="OneMessage-text">'+content;
     msg += '<i class="pull-right fa fa-thumbs-up"> '+likes+'</i>';
     msg += '</p></div></div>';
-    $("#messages").append(msg);
+    return msg;
   }
 
 // TIMELINE CODE
@@ -20,38 +22,79 @@ $(document).ready(function(){
       loaded = true;
     }
     $("#messages").html('<h1 id="title">Timeline<h1>');
-    console.log("titulo OK");
     e.preventDefault();
     $.getJSON("json/timeline.json",function(data){
-      console.log("getJSON");
       for (var i=0; i<data.messages.length; i++){
         img = data.messages[i].img;
         author = data.messages[i].author;
+        title = data.messages[i].title;
         date = data.messages[i].date;
         content = data.messages[i].content;
         likes = data.messages[i].likes;
-        create_msg(img,author,date,content,likes);
+        msg = create_msg(img,author,title,date,content,likes);
+        $("#messages").append(msg);
       }
-      console.log("fin");
     });
+    $.getJSON("json/update.json",function(data){
+      if (data.messages.length > 0){
+        newMsg = '<p class="new-messages">Se han encontrado ';
+        newMsg += data.messages.length;
+        newMsg += " nuevos mensajes.</p>";
+        $("#messages").prepend(newMsg);
+      }
+    });
+    myline = false;
   })
 
 // UPDATE CODE
-  $(".update").click(function(){
-    if (!loaded){
-      $("#messages").addClass("messages");
-      loaded = true;
+  $(".update").click(function(e){
+    if (myline){
+      $("#messages").removeClass("messages");
+      loaded = false;
     }
-    $("#messages").html('<h1 id="title">New messages loaded<h1>');
+    if (!loaded){
+      alert = '<div class="alert alert-danger"><strong>Error. </strong>'
+      alert += 'Debes visualizar tu timeline primero.</div>'
+      $("#messages").html(alert);
+    } else {
+      $("#title").html('New messages loaded');
+      e.preventDefault();
+      $.getJSON("json/update.json",function(data){
+        for (var i=0; i<data.messages.length; i++){
+          img = data.messages[i].img;
+          author = data.messages[i].author;
+          title = data.messages[i].title;
+          date = data.messages[i].date;
+          content = data.messages[i].content;
+          likes = data.messages[i].likes;
+          msg = create_msg(img,author,title,date,content,likes);
+          $("#title").after(msg);
+        }
+      });
+      $(".new-messages").remove();
+    }
   })
 
 // MY LINE CODE
-  $(".myline").click(function(){
+  $(".myline").click(function(e){
     if (!loaded){
       $("#messages").addClass("messages");
       loaded = true;
     }
     $("#messages").html('<h1 id="title">My line<h1>');
-  })
-
+    e.preventDefault();
+    $.getJSON("json/myline.json",function(data){
+      for (var i=0; i<data.messages.length; i++){
+        img = data.messages[i].img;
+        author = data.messages[i].author;
+        title = data.messages[i].title;
+        date = data.messages[i].date;
+        content = data.messages[i].content;
+        likes = data.messages[i].likes;
+        msg = create_msg(img,author,title,date,content,likes);
+        $("#messages").append(msg);
+      }
+    });
+    myline = true;
+  });
 });
